cfc26a08de88d53296f3ba5993d7760df649afc5
ishopjapan <thoitran2107@gmail.com>
2022-07-30 10:23:05 +0700

    thuc hanh object pool

---
+++ src/demoObjectPool/TaxiNotFoundException.java
@@ -0,0 +1,7 @@
+package demoObjectPool;
+
+public class TaxiNotFoundException extends RuntimeException{
+    public TaxiNotFoundException(String message) {
+        System.out.println(message);
+    }
+}
